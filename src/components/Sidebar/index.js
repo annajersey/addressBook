5fd272eef99b13f69582601900af7e7d88b4775a
Anna <anna.brodskaya@gmail.com>
2018-12-23 09:58:58 +0200

    added search filter

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,13 +4,37 @@ import {connect} from "react-redux";
 import {setCurrent} from "../../store/actions";
 
 class Sidebar extends React.Component {
+	constructor(props){
+		super(props);
+		this.state={
+			items: props.items
+		}
+	}
+	
+	componentDidUpdate(prevProps){
+		if (prevProps.items.length!=this.props.items.length) {
+			this.setState({items: this.props.items})
+		}
+	}
+	
+	filterItems(e){
+		let value = e.target.value;
+		let items = [...this.props.items]
+		items = items.filter(i=>
+			i.name.toLowerCase().indexOf(value.toLowerCase())==0||
+			i.lastname.toLowerCase().indexOf(value.toLowerCase())==0
+		)
+		console.log(value,items)
+		this.setState({items});
+	}
 	render() {
 		console.log(this.props.items)
 		return (
 			<aside>
 				<button onClick={()=>this.props.setCurrent(null)}>Add New</button>
+				<input type="text" onChange={(e)=>this.filterItems(e)}/>
 				<ul>
-					{this.props.items.map(p =>
+					{this.state.items.map(p =>
 						<li key={p.id} onClick={()=>this.props.setCurrent(p)}>{p.name + ' ' + p.lastname}</li>)
 					}
 				</ul>
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,7 +3,9 @@ import {ADD_PERSON, DELETE_PERSON, SET_CURRENT} from "../constants";
 const initialState = {
 	addressBookItems: [
 		{id: 1, name: "Test1", lastname: "Test2"},
-		{id: 2, name: "A1", lastname: "a1"}
+		{id: 2, name: "A1", lastname: "a1"},
+		{id: 3, name: "qwerty", lastname: "qwerty"},
+		{id: 4, name: "qwerty123", lastname: "qwerty"}
 	],
 	current: {id: 1, name: "Test1", lastname: "Test2"}
 	
@@ -14,23 +16,28 @@ export default (state = initialState, action) => {
 		case ADD_PERSON:
 			const {person} = action.payload
 			let newItemIndex = addressBookItems.findIndex(p => p.id === state.current.id)
-			if (newItemIndex<0) addressBookItems.push(person);
+			if (newItemIndex<0) {
+				person.id=getNextId(addressBookItems)
+				addressBookItems.push(person);
+			}
 			else addressBookItems[newItemIndex] = person
-			return {...state, addressBookItems};
+			return {...state, addressBookItems, current:createNewItem()};
 		case SET_CURRENT:
 			let {current} = action.payload
-			if (!current) current = createNewItem(addressBookItems)
+			if (!current) current = createNewItem()
 			return {...state, current};
 		case DELETE_PERSON:
 			let deleteItemIndex = addressBookItems.findIndex(p => p.id === state.current.id)
 			if(deleteItemIndex>-1) addressBookItems.splice(deleteItemIndex, 1);
-			return {...state, addressBookItems, current:createNewItem(addressBookItems)};
+			return {...state, addressBookItems, current:createNewItem()};
 		default:
 			return state;
 	}
 };
 
-
-const createNewItem = (addressBookItems) => {
-	return  {id: Math.max(...addressBookItems.map(p => p.id), 0) + 1, name: '', lastname: ''}
+const getNextId = (addressBookItems) => {
+	return Math.max(...addressBookItems.map(p => p.id), 0) + 1
+}
+const createNewItem = () => {
+	return  {id: parseFloat(Math.random()).toFixed(3), name: '', lastname: ''}
 }
